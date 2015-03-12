edc5055053c002a17c1810124f790e26d2d02fbe
NamNguyen52 <nam.nguyen52@gmail.com>
2015-03-11 21:58:24 -0700

    responsive

--- js/main.js
+++ js/main.js
@@ -24,57 +24,78 @@ $(document).ready(function(){
   // });
 });
 
+$("#test-go-moment-submit").click(function(){
+
+  setTimeout(function(){
+    $('.test-confirmation-container').toggleClass('clicked');
+  },200);
+  $('.call-to-action').toggleClass('clicked');
+  $('#test-go-moment').animate({'margin-left':'-400px'}, 200);
+
+});
+
+$("#email-submit-button").click(function(){
+
+  $('#email-input').animate({'margin-left':'-1000px'}, 200);
+  $('#email-submit-button').animate({'margin-left':'-1200px'}, 200);
+  $('#email-call-action').animate({'margin-left':'-1000px'}, 200);
+
+  setTimeout(function(){
+    $('.email-confirmation-container').toggleClass('clicked');
+  }, 300);
+});
+
 $(".feature-container").hover(function(){
 
 	$(this).children("div.feature-description").toggleClass("hovered");
 });
 
-$(document).ready(function() {
-  function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
+// $(document).ready(function() {
+//   function filterPath(string) {
+//   return string
+//     .replace(/^\//,'')
+//     .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+//     .replace(/\/$/,'');
+//   }
+//   var locationPath = filterPath(location.pathname);
+//   var scrollElem = scrollableElement('html', 'body');
  
-  $('a[href*=#]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top;
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 1000, function() {
-            location.hash = target;
-          });
-        });
-      }
-    }
-  });
+//   $('a[href*=#]').each(function() {
+//     var thisPath = filterPath(this.pathname) || locationPath;
+//     if (  locationPath == thisPath
+//     && (location.hostname == this.hostname || !this.hostname)
+//     && this.hash.replace(/#/,'') ) {
+//       var $target = $(this.hash), target = this.hash;
+//       if (target) {
+//         var targetOffset = $target.offset().top;
+//         $(this).click(function(event) {
+//           event.preventDefault();
+//           $(scrollElem).animate({scrollTop: targetOffset}, 1000, function() {
+//             location.hash = target;
+//           });
+//         });
+//       }
+//     }
+//   });
  
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
-});
+//   function scrollableElement(els) {
+//     for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+//       var el = arguments[i],
+//           $scrollElement = $(el);
+//       if ($scrollElement.scrollTop()> 0) {
+//         return el;
+//       } else {
+//         $scrollElement.scrollTop(1);
+//         var isScrollable = $scrollElement.scrollTop()> 0;
+//         $scrollElement.scrollTop(0);
+//         if (isScrollable) {
+//           return el;
+//         }
+//       }
+//     }
+//     return [];
+//   }
+// });
 
 function testGo() {
 	var guestNumber = document.getElementById('test-go-moment-input').value;
